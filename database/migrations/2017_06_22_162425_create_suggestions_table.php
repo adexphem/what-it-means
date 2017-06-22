981c6b3aa4427ab7c659b07500e9ba579d34dee3
Andela STC <andelastc@Andelas-MacBook-Pro-3.local>
2017-06-22 17:51:09 +0100

    Adding base files needed for the project and created some migration tables

---
+++ database/migrations/2017_06_22_162425_create_suggestions_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSuggestionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('suggestions', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('name_id')->unsigned();
+            $table->foreign('name_id')->references('id')->on('names');
+            $table->string('meaning');
+            $table->integer('user_id')->unsigned();
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->integer('status_id')->unsigned();
+            $table->foreign('status_id')->references('id')->on('statuses');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('suggestions');
+    }
+}
